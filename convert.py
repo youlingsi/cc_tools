5a44e371e4f3db92d28266ba694ed3371a882f4d
Leona Yang <youlingsi@Leonas-MacBook-Air.local>
2018-02-09 20:25:47 -0500

    Assignment2 Part 2

--- convert.py
+++ convert.py
@@ -1,4 +1,6 @@
 import cc_dat_utils
+import json
+import test_json_utils
 
 #Part 1
 #Use cc_data_utils.make_cc_data_from_dat() to load pfgd_test.dat
@@ -14,9 +16,13 @@ input_json_file = "data/test_data.json"
 
 ### Begin Add Code Here ###
 #Open the file specified by input_json_file
-#Use the json module to load the data from the file
+with open(input_json_file, "r") as jsFile:
+    #Use the json module to load the data from the file
+    jsData = json.load(jsFile)
 #Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
+gmLibrary = test_json_utils.make_game_library_from_json(jsData)
 #Print out the resulting GameLibrary data using print_game_library(game_library_data) in test_data.py
+print(gmLibrary)
 ### End Add Code Here ###
 
 
--- test_json_utils.py
+++ test_json_utils.py
@@ -1,17 +1,24 @@
 import test_data
-import json
 
 #Creates and returns a GameLibrary object(defined in test_data) from loaded json_data
 def make_game_library_from_json( json_data ):
     #Initialize a new GameLibrary
     game_library = test_data.GameLibrary()
-
+    
     #Loop through the json_data
+    for gm in json_data["gameList"]:
         #Create a new Game object from the json_data by reading
+        gmObj = test_data.Game()
         #  title
+        gmObj.title = gm["title"]
         #  year
+        gmObj.year = gm["Year"]
         #  platform (which requires reading name and launch_year)
+        gmPlatform = test_data.Platform()
+        gmPlatform.launch_year = gm["platform"]["launch year"]
+        gmPlatform.name = gm["platform"]["name"]
+        gmObj.platform =gmPlatform
         #Add that Game object to the game_library
+        game_library.add_game(gmObj)
     #Return the completed game_library
-
     return game_library
